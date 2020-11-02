f31eb4c454ebfeb90419fd873ded69470d492e31
Marti Belegu <48526054+mgbelegu@users.noreply.github.com>
2020-11-02 10:36:54 +0100

    Update css

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,7 +5,7 @@ import minidoLogo from '../../assets/images/Minido.png';
 const navigation = () => (
     <div className="Header">
         <img src={minidoLogo} alt="Minido Logo"/>
-        <h3 className="title">Minido</h3>
+        <h3 className="title">MINIDO</h3>
     </div>
 );
 
